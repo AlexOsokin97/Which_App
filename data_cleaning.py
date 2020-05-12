0bb81619abee528a3aa2cf8c8556a04983d6bcf2
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-05-12 20:56:57 +0300

    created more datasets

--- data_cleaning.py
+++ data_cleaning.py
@@ -63,7 +63,8 @@ tst = df_apps_clean.copy()
 tst = pd.get_dummies(tst, columns=["Category", "Content Rating", "Type"])
 tst.drop("Last Updated",axis=1, inplace=True)
 
-#importing KNN imputer and setting n_neighbors=33 (for each category)
+#importing KNN imputer
+#using different types of k for later testing
 from sklearn.impute import KNNImputer
 imputer = KNNImputer(n_neighbors=33)
 df_filled = imputer.fit_transform(tst)
@@ -71,10 +72,26 @@ df_filled = imputer.fit_transform(tst)
 #array to df
 ML_df = pd.DataFrame(data=df_filled)
 
+# K = 17
+imputer1 = KNNImputer(n_neighbors=17)
+df1_filled = imputer1.fit_transform(tst)
+
+ML_df1 = pd.DataFrame(data=df1_filled)
+
+# K = 67
+imputer2 = KNNImputer(n_neighbors=67)
+df2_filled = imputer2.fit_transform(tst)
+
+ML_df2 = pd.DataFrame(data=df2_filled)
+
+# K = 105
+imputer3 = KNNImputer(n_neighbors=105)
+df3_filled = imputer3.fit_transform(tst)
+
+ML_df3 = pd.DataFrame(data=df3_filled)
 #saving to csv:
 df_apps_clean.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/EDA_df.csv')
 ML_df.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model_df.csv')
-
-
-
-
+ML_df1.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model1_df.csv')
+ML_df2.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model2_df.csv')
+ML_df3.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model3_df.csv')
