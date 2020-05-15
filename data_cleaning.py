1885b45d00998de2beba47358c758a735f656ca4
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-05-15 10:34:40 +0300

    fixing the data cleaning code

--- data_cleaning.py
+++ data_cleaning.py
@@ -45,53 +45,54 @@ df_apps_clean['Size'] = df_apps_clean['Size'].apply(lambda x: -1 if x == 'Varies
 df_apps_clean['Size'] = df_apps_clean['Size'].str.replace('[M, k]', '')
 df_apps_clean['Size'] = pd.to_numeric(df_apps_clean['Size'])
 
-#finding the median size of each app category
-size_medians = df_apps_clean.groupby('Category')['Size'].median()
-
 #INSTALLS:
 df_apps_clean['Installs'] = df_apps_clean['Installs'].str.replace('[, +]', '')
 df_apps_clean['Installs'] = pd.to_numeric(df_apps_clean['Installs'])
 
 
-#RATING:
-#finding the median rating for each category
-rating_medians = df_apps_clean.groupby(['Category'])['Rating'].median()
 
-#creating a copy of the data set, setting dummy variables for the categorical data 
+
+
 #and importing knn imputer to fill the nan values 
 tst = df_apps_clean.copy()
-tst = pd.get_dummies(tst, columns=["Category", "Content Rating", "Type"])
-tst.drop("Last Updated",axis=1, inplace=True)
+
+categorical_feature_mask = tst.dtypes=='category'
+# filter categorical columns using mask and turn it into a list
+categorical_cols = tst.columns[categorical_feature_mask].tolist()
+
+# import labelencoder
+from sklearn.preprocessing import LabelEncoder
+# instantiate labelencoder object
+le = LabelEncoder()
+# apply le on categorical feature columns
+tst[categorical_cols] = tst[categorical_cols].apply(lambda col: le.fit_transform(col))
+
+
+
+
+
+
+
+
+
+
 
 #importing KNN imputer
 #using different types of k for later testing
-from sklearn.impute import KNNImputer
-imputer = KNNImputer(n_neighbors=33)
-df_filled = imputer.fit_transform(tst)
+#from sklearn.impute import KNNImputer
+#imputer = KNNImputer(n_neighbors=33)
+#df_filled = imputer.fit_transform(tst)
 
 #array to df
-ML_df = pd.DataFrame(data=df_filled)
-
-# K = 17
-imputer1 = KNNImputer(n_neighbors=17)
-df1_filled = imputer1.fit_transform(tst)
-
-ML_df1 = pd.DataFrame(data=df1_filled)
+#ML_df = pd.DataFrame(data=df_filled)
 
 # K = 67
-imputer2 = KNNImputer(n_neighbors=67)
-df2_filled = imputer2.fit_transform(tst)
+#imputer2 = KNNImputer(n_neighbors=67)
+#df2_filled = imputer2.fit_transform(tst)
 
-ML_df2 = pd.DataFrame(data=df2_filled)
+#ML_df2 = pd.DataFrame(data=df2_filled)
 
-# K = 105
-imputer3 = KNNImputer(n_neighbors=105)
-df3_filled = imputer3.fit_transform(tst)
-
-ML_df3 = pd.DataFrame(data=df3_filled)
 #saving to csv:
-df_apps_clean.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/EDA_df.csv')
-ML_df.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model_df.csv')
-ML_df1.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model1_df.csv')
-ML_df2.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model2_df.csv')
-ML_df3.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model3_df.csv')
+#df_apps_clean.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/EDA_df.csv')
+#ML_df.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model_df.csv')
+#ML_df2.to_csv('C:/Users/User/Documents/GitHub/Which_App_Category/Model2_df.csv')
