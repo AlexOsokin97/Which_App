25b5afc3519184d0f0f38fdf12fd5f83bdc4306b
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-05-22 13:44:32 +0300

    3 different ANN and 3 plots

--- ANN.py
+++ ANN.py
@@ -4,7 +4,6 @@ Created on Mon May 18 19:05:20 2020
 
 @author: Alexander
 """
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -47,22 +46,106 @@ from keras.layers import Dense
 neural = Sequential()
 
 #creating NN input layer
-neural.add(Dense(activation="relu", input_dim=39, units=20, kernel_initializer="normal"))
+neural.add(Dense(activation="relu", input_dim=39, units=20, kernel_initializer="he_uniform"))
 
 #creating the NN hidden layers
-neural.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="normal"))
-neural.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="normal"))
-neural.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="normal"))
+neural.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+neural.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+neural.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+neural.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
 
 #creating NN output layer
-neural.add(Dense(activation="linear", input_dim=20, units=1, kernel_initializer="normal"))
+neural.add(Dense(activation="linear", input_dim=20, units=1, kernel_initializer="he_uniform"))
 
 #compiling the ANN
-neural.compile(optimizer = 'sgd', loss='mse', metrics=['mae'])
+neural.compile(optimizer = "adagrad" , loss="mean_squared_logarithmic_error" , metrics=['mse'])
 
 #fitting the model
-neural.fit(X_train, y_train, batch_size=10, nb_epoch=100)
+history = neural.fit(X_train, y_train, validation_data=(X_test,y_test), batch_size=10, epochs=100, verbose=0)
 
-y_predict = neural.predict(X_test)
+# evaluate the model
+train_mse = neural.evaluate(X_train, y_train, verbose=0)
+test_mse = neural.evaluate(X_test, y_test, verbose=0)
+
+# plot loss during training
+plt.subplot(211)
+plt.title('Loss')
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
+# plot mse during training
+plt.subplot(212)
+plt.title('Mean Squared Error')
+plt.plot(history.history['mse'], label='train')
+plt.plot(history.history['val_mse'], label='test')
+plt.legend()
+plt.show()
 
 
+###############################################################################################################################
+
+#initializing the ANN
+model = Sequential()
+
+#creating NN input layer
+model.add(Dense(activation="relu", input_dim=39, units=20, kernel_initializer="he_uniform"))
+          
+#creating the NN hidden layers
+model.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+model.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+
+#creating NN output layer          
+model.add(Dense(activation="linear", input_dim=20, units=1, kernel_initializer="he_uniform"))
+
+#compiling the ANN
+model.compile(optimizer = "sgd" , loss="mean_squared_error")
+
+# fit model
+history2 = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, verbose=0)
+
+# evaluate the model
+train_mse_model = model.evaluate(X_train, y_train, verbose=0)
+test_mse_model = model.evaluate(X_test, y_test, verbose=0)
+
+# plot loss during training
+plt.title('Loss / Mean Squared Error')
+plt.plot(history2.history['loss'], label='train')
+plt.plot(history2.history['val_loss'], label='test')
+plt.legend()
+plt.show()
+
+#######################################################################################################################################
+
+#initializing the ANN
+model2 = Sequential()
+
+#creating NN input layer
+model2.add(Dense(activation="relu", input_dim=39, units=20, kernel_initializer="he_uniform"))
+          
+#creating the NN hidden layers
+model2.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+model2.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+model2.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+model2.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+model2.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+model2.add(Dense(activation="relu" , input_dim=20, units=20, kernel_initializer="he_uniform"))
+
+#creating NN output layer          
+model2.add(Dense(activation="linear", input_dim=20, units=1, kernel_initializer="he_uniform"))
+
+#compiling the ANN
+model2.compile(optimizer = "sgd" , loss="mean_absolute_error", metrics=['mse'])
+
+# fit model
+history3 = model2.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, verbose=0)
+
+# evaluate the model
+train_mse_model2 = model2.evaluate(X_train, y_train, verbose=0)
+test_mse_model2 = model2.evaluate(X_test, y_test, verbose=0)
+
+# plot loss during training
+plt.title('Loss')
+plt.plot(history2.history['loss'], label='train')
+plt.plot(history2.history['val_loss'], label='test')
+plt.legend()
+plt.show()
